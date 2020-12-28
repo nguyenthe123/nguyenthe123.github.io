9206231e373fd1b87ed43aa99124b25fa34825e0
nguyenthe123 <duythe.276051@gmail.com>
2020-12-28 22:06:38 +0700

    third cm

--- upgit.py
+++ upgit.py
@@ -3,10 +3,11 @@ import os
 htmlfile = "word.html"
 afile = open(htmlfile, 'r')
 lines = afile.readlines()
+fout = open("index.html",'w+')
 for line in lines:
     if line[0:65] == ' <img src=/home/kepthe/Desktop/Gitne/trans/nguyenthe123.github.io':
         line = line[0:10] + line[65:len(line)]
-    fout = open('index.html','a')
+        print(line)
     fout.write(line)
-    fout.close
 afile.close
+fout.close
